cd595757a59032f1f56a242bcae27cad1cb2eed9
Emma Antonucci <emmanicole0920@gmail.com>
2022-10-27 10:32:28 -0600

    modified:   three-tier-master/datagrid.js

--- three-tier-master/datagrid.js
+++ three-tier-master/datagrid.js
@@ -1,6 +1,6 @@
 // URL to your production code when you publish yoru google app script as a web app
 // You will get this URL when you publish your google apps script as a web app
-let prefix="https://script.google.com/macros/s/AKfycbzY2POlaXyzqYscnsvbcaiwKLSdrHx-1BRA3Eu5ukUso_mn-7KVyU34JcR_A2a16d3i/exec" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
+let prefix="https://script.google.com/macros/s/AKfycbxmH3Te_lqAspOJPGOf8aogcn4ImErf1yy8vvcIVMaQeEOmraOg-Oxa4E1y_sPhr7x7/exec" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
 const dealy_seconds = 4 // how long data validation messages are visible
 
 //example call: file:///C:/Users/Gove/three-tier/datagrid.html?employee
